340c7784708a8d5168de52d02b57f900489fb72b
Rick Park <ricksmp95@gmail.com>
2023-07-04 11:14:23 +0100

    Add initial server setup.

---
+++ server/index.js
@@ -0,0 +1,46 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import helmet from "helmet";
+import mongoose from "mongoose";
+import morgan from "morgan";
+
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+/* CONFIG */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.port ?? 9000;
+
+try {
+  await mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+} catch (e) {
+  console.log(`${e} did not connect`);
+  throw e;
+}
+
+app.listen(PORT, () => {
+  console.log(`Server Port: ${PORT}`);
+});
